cfa1803952c9a76bc0f524d6fcb6ca10b9360aa5
Kevin Akmal <kevin.saanin@binus.ac.id>
2024-05-05 23:15:13 +0700

    Added array of objects to users

--- src/favourites/favourites.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FavouritesController } from './favourites.controller';
-import { FavouritesService } from './favourites.service';
-
-@Module({
-  controllers: [FavouritesController],
-  providers: [FavouritesService]
-})
-export class FavouritesModule {}
