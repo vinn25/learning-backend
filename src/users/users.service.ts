61346d6fc7128b023be70e8fd07af43825a77417
Kevin Akmal <kevin.saanin@binus.ac.id>
2024-05-05 23:52:48 +0700

    Updated getAllFavourites

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -36,7 +36,46 @@ export class UsersService {
     ]
 
     // Get all the users
-    getAllUsers() {
+    getAllUsers(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+        if (role) {
+            return this.users.filter(user => user.role === role)
+        }
+        return this.users
+    }
+
+    // Get a user specified by id
+    getUser(id: number) {
+        const user = this.users.find(user => user.id === id)
+
+        return user
+    }
+
+    // Create a new user
+    createUser(user: { name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+        const usersByHighestId = [...this.users].sort((a, b) => b.id = a.id)
+        const newUser = {
+            id: usersByHighestId[0].id + 1,
+            ...user
+        }
+        this.users.push(newUser)
+        return newUser
+    }
+
+    updateUser(id: number, userUpdate: { name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+        this.users = this.users.map(user => {
+            if (user.id === id) {
+                return { ...user, ...userUpdate }
+            }
+            return user
+        })
+        return this.getUser(id)
+    }
+
+    deleteUser(id: number) {
+        const removedUser = this.getUser(id)
+
+        this.users = this.users.filter(user => user.id !== id)
+
         return this.users
     }
 }
