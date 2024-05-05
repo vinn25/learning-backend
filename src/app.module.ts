3fce3d3b38f223bcd46b59bdf57b470fb801f5c8
Kevin Akmal <kevin.saanin@binus.ac.id>
2024-05-05 23:52:48 +0700

    Updated getAllFavourites

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { FavouritesModule } from './favourites/favourites.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, FavouritesModule],
 })
 export class AppModule { }
